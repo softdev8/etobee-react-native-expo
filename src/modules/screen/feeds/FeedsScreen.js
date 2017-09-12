a323dfc62e2b7c409e15e27690a92d32312ecc53
oncemore <oncemoreforyou@outlook.com>
2017-09-13 07:22:51 +0900

    facebook id change

---
+++ src/modules/screen/feeds/FeedsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+class FeedsScreen extends React.Component {
+    render() {
+        return (
+            <View />
+        )
+    }
+}
+
+export default FeedsScreen
